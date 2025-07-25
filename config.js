11f803574e70ac61182d18e10a2eed04ee01aa8f
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-07-25 10:25:17 +0530

    Remove Stripe secret key

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config();
-
- const STRIPE_SECRET_KEY= process.env.STRIPE_SECRET_KEY || "sk_test_51RRc4L2fbpYXk6FHq33Ref4T5RIgGrXvwZ95wx5ej6hmbXQSQoi3uOvAOSrmzv2Pu2cTQKE3uLv2qsqMQOKnMXX400nysiosDi";
+ const STRIPE_SECRET_KEY= process.env.STRIPE_SECRET_KEY 
  
 export default{
 STRIPE_SECRET_KEY
