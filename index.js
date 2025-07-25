55f601b2606437f4c3dd0dcefc3cd4333312609e
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-07-25 11:26:31 +0530

    Fix button bug and update styles

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(fileUpload({
 
  // Cors middleware
  app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND ,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [ "Content-Type", "Authorization"]
