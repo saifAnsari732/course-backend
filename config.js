79f157f0d7bad2a7d8f116f297d95fd34901c4a0
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-07-25 09:43:20 +0530

    Removed secret key from config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
- const STRIPE_SECRET_KEY="sk_test_51RRc4L2fbpYXk6FHq33Ref4T5RIgGrXvwZ95wx5ej6hmbXQSQoi3uOvAOSrmzv2Pu2cTQKE3uLv2qsqMQOKnMXX400nysiosDi"
+ const STRIPE_SECRET_KEY= process.env.STRIPE_SECRET_KEY || "sk_test_51RRc4L2fbpYXk6FHq33Ref4T5RIgGrXvwZ95wx5ej6hmbXQSQoi3uOvAOSrmzv2Pu2cTQKE3uLv2qsqMQOKnMXX400nysiosDi";
  
 export default{
 STRIPE_SECRET_KEY
