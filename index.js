f73cd8ab4d09a569b616f7fd3e781dc06e42cead
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-07-27 14:18:32 +0530

    lastcomit

--- index.js
+++ index.js
@@ -29,16 +29,12 @@ app.use(fileUpload({
   tempFileDir : '/tmp/'
 }));
 
-
- // Cors middleware
- app.use(cors({
-  origin: process.env.FRONTEND ,
+app.use(cors( {
+  origin: process.env.FRONTEND,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: [ "Content-Type", "Authorization"]
-}))
-
-
+  allowedHeaders: ["Content-Type", "Authorization"]
+} ));
 
 app.get('/', (req, res) => {
   res.send('Welcome to Course app')
